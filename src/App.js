b799a76f9e332cda7c188c8e316c424cc76c508c
mohammedalsamki <momosamak21@gmail.com>
2021-09-18 18:41:54 -0400

    edit my api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import Location from './components/Location';
 import SearchForm from './components/SearchForm';
@@ -6,6 +5,8 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Movies from './components/Movies';
 import { Form, Button, Card, Table, CardColumns } from 'react-bootstrap/';
+import './App.css';
+
 
 export class App extends Component {
   constructor(props) {
@@ -16,33 +17,34 @@ export class App extends Component {
       lat: "",
       lon: "",
       showData: false,
+      moviesInfor: [],
       showmovies: false,
-      movieError:'',
+      movieError: '',
     }
   }
-  viewData = async (e) => {
-    e.preventDefault();
-    let cityName = e.target.userInput.value;
+  // viewData = async (e) => {
+  //   e.preventDefault();
+  //   let cityName = e.target.userInput.value;
 
 
-    let movieDataInformation = `${process.env.REACT_APP_URL}/movies?city=${cityName}`;
+  //   let movieDataInformation = `${process.env.REACT_APP_URL}/movies?city=${cityName}`;
 
-    try{
-      let moviesValu = await axios.get(movieDataInformation);
-      console.log(moviesValu)
-      this.setState({
-        moviesInfor: moviesValu.data,
-        showmovies: true
-      })
-      console.log(this.state.moviesInfor)
-    }
-    catch(err){
-      this.setState({
-        movieError: err,
-        showmovies: false
-      })
-    }
-  }
+  //   try{
+  //     let moviesValu = await axios.get(movieDataInformation);
+  //     console.log(moviesValu)
+  //     this.setState({
+  //       moviesInfor: moviesValu.data,
+  //       showmovies: true
+  //     })
+  //     console.log(this.state.moviesInfor)
+  //   }
+  //   catch(err){
+  //     this.setState({
+  //       movieError: err,
+  //       showmovies: false
+  //     })
+  //   }
+  // }
 
   handleLocation = (e) => {
     let city_name = e.target.value;
@@ -51,29 +53,53 @@ export class App extends Component {
     })
   }
   handleSubmit = (e) => {
-    console.log(`${process.env.REACT_APP_LOCATIONIQ_API_KEY}`);
-    e.preventDefault();
-    let config = {
-      method: "GET",
-      baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=pk.c478560a12e47d17aa9e3454ee3eb63e&q=${this.state.city_name}`
-    }
-    axios(config).then(res => {
-      let responseData = res.data[0]
-      this.setState({
-        city_name: responseData.display_name,
-        lon: responseData.lon,
-        lat: responseData.lat,
-        type: responseData.type,
-        showData: true
+    if (this.state.city_name !== '') {
 
+      // console.log(`${process.env.REACT_APP_LOCATIONIQ_API_KEY}`);
+      e.preventDefault();
+      let config = {
+        method: "GET",
+        baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=pk.c478560a12e47d17aa9e3454ee3eb63e&q=${this.state.city_name}`
+      }
+      axios(config).then(res => {
+        let responseData = res.data[0]
+        this.setState({
+          city_name: responseData.display_name,
+          lon: responseData.lon,
+          lat: responseData.lat,
+          type: responseData.type,
+          showData: true
+
+        })
       })
-    })
+        .then(() => {
+          axios.get(`https://city-explore-api.herokuapp.com/movies?city${this.state.city_name}`).then(
+            res => {
+              this.setState({
+                showmovies: true,
+                moviesInfor: res.data
+
+              })
+            }
+
+          )
+        })
+      this.setState({ movieError: false })
+    }
+    else {
+      this.setState({
+        movieError: true
+      })
+
+    }
   };
-  render(){
+        
+
+  render() {
     return (
       <div className='main'>
         <h1>Welcome to City explorer</h1>
-        <SearchForm className='SearchForm' handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} viewData={this.viewData}/>
+        <SearchForm className='SearchForm' handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} viewData={this.viewData} />
         {
           this.state.showData &&
           <Location className='Location' city_name={this.state.city_name}
@@ -84,23 +110,38 @@ export class App extends Component {
           />
         }
         <Card.Img className='cardImg' src={`https://maps.locationiq.com/v3/staticmap?key=pk.c478560a12e47d17aa9e3454ee3eb63e&center=${this.state.lat},${this.state.lon}`} alt="Card image" height='500px' />
-        <CardColumns>
-          {this.state.showmovies &&
-          this.state.moviesInfor.map(item=>(
-            <Movies 
-            Title={item.title}
-            description={item.overview}
-            imgURL={item.image_ur}
-            data={item.released_on}
-            vote={item.average_votes}
+      
+          {/* {this.state.showmovies &&
+            this.state.moviesInfor.map(item => (
+              <Movies
+                Title={item.title}
+                description={item.overview}
+                imgURL={item.image_ur}
+                data={item.released_on}
+                vote={item.average_votes}
+              />
+            ))}
+        </CardColumns> */}
+
+
+
+
+        {/* <div className='row' >
+
+          {this.state.showmovies && this.state.moviesInfor.map(value => {
+            return <Movies title={value.title}
+              Title={value.title}
+              description={value.overview}
+              imgURL={value.image_ur}
+              data={value.released_on}
+              vote={value.average_votes}
             />
-          ))}
-        </CardColumns>  
-         
-            {this.state.movieError && <p>error in getting data</p>}
+          })
+          }
+          {this.state.movieError && <p>error in getting data</p>}
 
-
-
+        </div> */}
+       {this.state.showmovies && <Movies moviesInfor={this.state.moviesInfor}/>}
 
 
       </div>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Col } from 'react-bootstrap'
+
 
 export class Movies extends Component {
     render() {
         return (
-            <div>
-                <p>{this.props.Title}</p>
-            <p>{this.props.description}</p>
-            <p>{this.props.data}</p>
-            <img src={this.props.imgURL} alt="Movies" width="400" height="500"></img>
-            </div>
+            <Col>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="" />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            <p>{this.props.title}</p>
+                            <p>{this.props.description}</p>
+                            <p>{this.props.data}</p>
+                            <p>{this.props.average_votes}</p>
+                            <img src={this.props.image_ur} alt={this.props.title} width="400" height="500"></img>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
         )
     }
 }
